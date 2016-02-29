224d10f967ff7cd6f1366451a119426c994d9c0d
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-02-29 12:24:49 +0100

    Add sorting for two merged lists with different keys

--- list_of_unique_dicts_by_value.py
+++ list_of_unique_dicts_by_value.py
@@ -20,4 +20,25 @@ list_of_dict = [
 ]
 
 sorted_list = {v['id']: v for v in list_of_dict}.values()
-print sorted_list
+print "sorted list is " sorted_list
+
+"""
+If you have two lists and you want to merge those lists and sort with different keys
+"""
+
+L1 = [
+    {'id': 1, 'name': 'john', 'age': 34},
+    {'id': 1, 'name': 'john', 'age': 34},
+    {'id': 2, 'name': 'hanna', 'age': 30},
+    {'id': 1, 'name': 'john', 'age': 34},
+]
+
+L2 = [
+{'sn': 2, 'name': 'hanna', 'age': 30} ,
+{'sn': 3, 'name': 'han', 'age': 30},
+{'sn': 2, 'name': 'hanna', 'age': 30},
+]
+L3 = L1 + L2
+
+sorted_merged_list = {v.get('id') or v.get('sn'): v for v in L3}.values()
+print "Sorted merge list is, " sorted_merged_list
