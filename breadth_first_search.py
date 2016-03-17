1c845238a9fd3bda20bec9c07fb92328130e6c0d
Kosy Anyanwu <kosy.anyanwu@andela.com>
2016-03-17 15:26:51 +0100

    Add breadth first search

---
+++ breadth_first_search.py
@@ -0,0 +1,128 @@
+test_input = """5 4
+1 2
+2 3
+3 5
+1 4
+1"""
+
+data = [map(int, line.split()) for line in test_input.splitlines()]
+# [[4,2],[1,2],[1,3],[1]]
+#    0     1     2    3
+#         [.......]
+# [1:M+1] = [1:3]
+
+# 1S   2     4
+# O----O     O
+# |
+# |
+# 3
+
+
+class Node(object):
+    def __init__(self, number):
+        self.neighbours = []
+        self.distance_from_start = -1
+        self.number = number
+        self.is_start = False
+
+N = data[0][0]
+M = data[0][1]
+
+# take N and M from the first line
+# create a list of N Node objects
+nodes = [Node(i + 1) for i in xrange(N)]
+
+for edge in data[1:M+1]:
+    # data[1:M+1] = [[1,2],[1,3]]
+    # edge = [1,2]
+    x, y = edge  # this edge connects node x with node y
+    nodes[x - 1].neighbours.append(nodes[y - 1])
+    nodes[y - 1].neighbours.append(nodes[x - 1])
+
+S = data[-1][0]
+
+start_node = nodes[S - 1]
+start_node.is_start = True
+start_node.distance_from_start = 0
+
+for i, node in enumerate(nodes):
+    print "Node", i + 1
+    print "Neighbours:", ", ".join(str(neighbour.number) for neighbour in node.neighbours)
+    if node.is_start:
+        print "This is the start node"
+    print
+
+node_queue = [start_node]
+
+while len(node_queue) > 0:
+    print "queue = [" + ", ".join(str(node.number) for node in node_queue) + "]"
+    current_node = node_queue.pop()
+    print "Checking node", current_node.number, "- Distance from start node:",
+    current_node.distance_from_start
+
+    current_node_neighbours = current_node.neighbours
+    for neighbour in current_node_neighbours:
+        print "  Neighbour: ", neighbour.number
+        if neighbour.distance_from_start < 0:
+            neighbour.distance_from_start = current_node.distance_from_start + 6
+            print "  Neighbour's distance from start:", neighbour.distance_from_start
+            node_queue.insert(0, neighbour)
+            print "  adding node", neighbour.number, "to queue"
+        else:
+            print "  Skipping because this node has been reached before"
+
+print
+for node in nodes:
+    print "Node", node.number, "has distance", node.distance_from_start,
+    "from the start"
+
+# Checking node 3 - Distance from start node: 12
+#  Neighbour:  2
+#  Skipping because this node has been reached before
+#  Neighbour:  5
+#  Neighbour's distance from start: 18
+#  adding node 5 to queue
+
+"""5 4
+1 2
+2 3
+3 5
+1 4
+1"""
+
+# 1    2    3    5
+# O----O----O----O
+# |
+# |
+# |
+# O 4
+
+# 1 is start node
+# checking node 1 - distance from start: 0
+#   Neighbour: 2
+#      Neighbour's distance from start: 6
+#      adding node 2 to queue
+#   Neighbour: 4
+#      Neighbour's distance from start: 6
+#      adding node 4 to queue
+#
+# queue = [node2, node4]
+
+# next while loop iteration:
+# checking node 2 - distance from start: 6
+#   Neighbour: 3
+#      Neighbour's distance from start: 12
+#      adding node 3 to the queue
+#   Neighbour: 1
+#      skipping because this node has been reached before
+
+# queue = [node4, node3]
+# .... handle node 4 ....
+# queue = [node3]
+
+# checking node 3 - distance from start: 12
+#   Neighbour: 5
+#      Neighbour's distance from start: 18
+#      adding node 3 to the queue
+#   Neighbour: 2
+#      skipping because this node has been reached before
